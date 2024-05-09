4baabedce4efee0e60a7560e63a2a3635e4aff0b
JoeMuldowney <muldowneyjoe84@gmail.com>
2024-05-09 08:09:34 -0400

    adding server to allowed hosts

--- userauthsite/userauthsite/settings.py
+++ userauthsite/userauthsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.220.94.221','localhost']
+ALLOWED_HOSTS = ['18.220.48.41','localhost']
 
 
 # Application definition
