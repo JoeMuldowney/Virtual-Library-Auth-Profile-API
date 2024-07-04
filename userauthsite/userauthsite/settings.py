a20ddae9ff75f017be0e958c73cd6611c839a723
JoeMuldowney <muldowneyjoe84@gmail.com>
2024-07-03 22:15:06 -0400

    jenkinsfile update

--- userauthsite/userauthsite/settings.py
+++ userauthsite/userauthsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['18.116.29.111', '18.220.48.41', 'localhost']
 
 
 # Application definition
